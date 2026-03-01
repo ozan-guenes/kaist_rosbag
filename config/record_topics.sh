64c625a6d6affc2db6217769cb8f3caa015e8a1a
Ozan Günes <ozan.guenes@kaist.ac.kr>
2026-03-01 18:36:42 +0900

    Docking: update record_topics.sh, .gitignore

--- config/record_topics.sh
+++ config/record_topics.sh
@@ -1,33 +1,48 @@
 rosbag record \
+/tf \
+/tf_static \
+/clock \
+/diagnostics \
+/rosout \
+/navsat/fix \
+/mavros/state \
+/mavros/extended_state \
+/mavros/statustext/recv \
+/mavros/imu/data_raw \
+/mavros/imu/data \
+/mavros/local_position/pose \
+/mavros/local_position/odom \
+/mavros/local_position/velocity_local \
+/mavros/setpoint_velocity/cmd_vel \
+/mavros/vision_pose/pose \
+/mavros/odometry/out \
+/uav/camera/color/camera_info \
+/uav/camera/color/image_raw/compressed \
+/t265/camera/odom/sample \
+/t265/vio_watchdog/state \
+/t265/vio_watchdog/healthy \
+/endo_cam/camera_info \
+/endo_cam/image_raw/compressed \
+/tag_detections \
+/tag_detections_image/compressed \
 /docking/command \
+/docking/dock_estimate \
+/docking/dock_measurement/front \
+/docking/gates \
 /docking/lock_command \
 /docking/state \
 /docking/target_pose \
 /docking/uav_path \
-/docking/path_marker \
-/docking/state_marker \
-/docking/target_arrow \
-/docking/target_cube \
-/uav/arrow_marker \
-/uav/mesh_marker \
-/endoscope_cam/camera_info \
-/endoscope_cam/image_raw \
-/mavros/imu/data \
-/mavros/local_position/pose \
-/mavros/local_position/velocity_local \
-/mavros/setpoint_velocity/cmd_vel \
-/mavros/vision_pose/pose \
-/mavros/state \
-/navsat/fix \
-/tag_detections \
-/tag_detections_image/compressed \
-/uav/realsense_D435i/color/camera_info \
-/uav/realsense_D435i/color/image_raw/compressed \
-/uav/realsense_D435i/depth/camera_info \
-/uav/realsense_D435i/depth/image_raw \
-/tf \
-/clock \
-/jackal_velocity_controller/cmd_vel \
-/jackal_velocity_controller/odom \
 /odometry/filtered \
 -o ${MISSION_NAME}
+
+# /docking/uav_path \ ----
+# /docking/path_marker \
+# /docking/state_marker \
+# /docking/target_arrow \
+# /docking/target_cube \
+# /uav/arrow_marker \
+# /uav/mesh_marker \
+
+# /uav/camera/depth/camera_info \
+# /uav/camera/depth/image_raw \
